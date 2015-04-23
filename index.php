5535f489ce26aee642bad4121848878e9cedb540
Alex Bass <alexbassy@users.noreply.github.com>
2015-04-23 15:57:32 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,330 @@
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="initial-scale=1">
+    <title>Q-municate - IM and video calling in the same open-source app</title>
+	<link rel="stylesheet" href="normalize.min.css" />
+	<link rel="stylesheet" href="bootstrap-grid.min.css" />
+	<link rel="stylesheet" href="animate.min.css" />
+	<link rel="stylesheet" href="style.css" />
+	
+	<script type="text/javascript" src="retina.min.js"></script>
+	<script type="text/javascript" src="//use.typekit.net/qvu3wjx.js"></script>
+	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+</head>
+<body>
+	<div class="content-wrap">
+		<header id="header-wrap">
+		    <div id="header-content-wrap" class="container">
+		        <div class="row">
+	    			<div class="col-md-5">
+	    				<div id="large-iphone-wrapper">
+	    					<img src="images/header-iphone.png" alt="Q-municate chat on an iPhone" id="large-iphone"/>
+	    					<div id="platforms-mobile">
+						    	<img src="images/v/apple.svg" alt="Apple icon" class="header-platforms" data-no-retina/>
+						    	<img src="images/v/android.svg" alt="Android icon" class="header-platforms" data-no-retina/>
+						    	<img src="images/v/web.svg" alt="Web/globe icon" class="header-platforms" data-no-retina/>
+					    	</div>
+	    				</div>
+					</div>
+	    			<div class="col-md-7">
+					    <section id="welcome" class="animated fadeInUp">
+					    	<div id="platforms-desktop">
+						    	<img src="images/v/apple.svg" alt="Apple icon" class="header-platforms" data-no-retina/>
+						    	<img src="images/v/android.svg" alt="Android icon" class="header-platforms" data-no-retina/>
+						    	<img src="images/v/web.svg" alt="Web/globe icon" class="header-platforms" data-no-retina/>
+					    	</div>
+					    	
+							<p>The open-source, <br class="dk-only">cross platform video calling and <br class="dk-only">instant messaging app from Quickblox!</p>
+							
+							<img src="images/logo_web.png" alt="Q-municate: Your own chat" id="qm-logo"/>
+							<div style="display:none">
+								<h1>Q-municate</h1>
+								<h3>Your own chat app</h3>
+							</div>
+							
+							<div id="getButtons">
+								<!-- <a href="#register" class="join-beta">Join the beta</a> -->
+								<a href="https://itunes.apple.com/us/app/q-municate/id909698517?mt=8" class="appstore-button"><img src="/images/appstore.svg" alt="Apple App Store"/></a>
+								<a href="https://play.google.com/store/apps/details?id=com.quickblox.q_municate" class="appstore-button"><img src="/images/playstore.png" alt="Google Play Store"/></a>
+							</div>
+							
+						</section>
+					</div>
+				</div>
+		    </div>
+		</header>
+		
+		<div id="chat-wrap">
+		    <div class="container">
+		        <div class="row">
+            		<div class="col-md-8">
+		        		<section id="chat-content" class="content">
+		            		<h1>Chat</h1>
+		            		<p>Q-municate comes with powerful instant messaging right out of the box. Powered by the flexible XMPP protocol and Quickblox signalling technologies, with compatibility for server-side chat history, group chats, attachments and user avatars, it's pretty powerful. It also has chat bubbles and user presence (online/offline).</p>
+			        		<div id="chat-features" class="row">
+			            		<div class="col-md-4">
+			            		    <img src="images/v/user.svg" alt="feature_name" data-no-retina/>
+			            		    <h4>Users</h4>
+			                        <p>Q-municate users can sign-up with just a Facebook account. They can also update their profile picture, status message, contact details and avatar.</p>
+			                    </div>
+			            		<div class="col-md-4">
+			            		    <img src="images/v/media.svg" alt="feature_name" data-no-retina/>
+			            		    <h4>Media</h4>
+			                        <p>Send photos, videos and voice recordings with ease and with no limits - these files are stored safely in the Quickblox cloud.</p>
+			            		</div>
+			            		<div class="col-md-4">
+			            		    <img src="images/v/group.svg" alt="feature_name" data-no-retina/>
+			            		    <h4>Groups</h4>
+			                        <p>Whether it's the football team or your family, creating a group chat is so simple that your grandma could do it (and if she does, it's easy to leave too)</p>
+			            		</div>
+			        		</div>
+		        		</section>
+		    		</div>
+		    		<div class="col-md-4">
+	        		    <div id="chat-iphone-container">
+	            		    <img src="images/chat-iphone.png" alt="Full length iPhone with chat screen on Q-municate app for iOS" id="chat-iphone">
+	        		    </div>
+	        		</div>
+			    </div>
+		    </div>
+		</div>
+
+		<div id="video-wrap">
+		    <div class="container">
+		        <div class="row">
+	        		<div class="col-md-4">
+		    			<div id="video-iphone-wrap">
+		    		    	<img src="images/video-iphone.jpg" alt="A video call between a guy and a girl" id="video-iphone"/>
+		    			</div>
+		    		</div>
+		    		<div class="col-md-8">
+		        		<section id="video-content" class="content">
+		                    <h1>Video calling</h1>
+		                    <p>One of the coolest technologies of the 21st century, Q-municate has HD video calling powered by WebRTC right out of the box. It's peer-to-peer to reduce latency and bandwidth and has a simple user interface that you'd expect from the tech giants - easy camera switching, muting and soon the ability to record video.</p>
+		                    <section id="cross-platform">
+		                        <img src="images/v/phone.svg" alt="Phone icon" data-no-retina/>
+		                        <img src="images/v/laptop.svg" alt="Laptop icon" data-no-retina/>
+		                        <img src="images/v/tablet.svg" alt="Tablet icon" data-no-retina/>
+		                        <h3>The cross-platform solution</h3>
+		                        <p>Q-municate uses WebRTC for video and audio calling meaning it’s a great cross-platform solution. WebRTC itself it open-source, so you can modify the code as much as you wish, or you can trust the thousands of skilled developers who contributed to it’s development.</p>                        
+		                    </section>
+		        		</section>
+		    		</div>
+			    </div>
+		    </div>
+		</div>
+		
+		<div id="push-wrap">
+		    <div class="container">
+		        <div class="row">
+		    		<div class="col-md-8">
+						<section id="push-content-wrap" class="content">
+							<h1>Push notifications</h1>
+							<p>Instant, easy and free notifications are fully integrated into both the iOS and Android versions of Q-municate. Receive notifications for chat messages, incoming calls and any other custom alert that you want to send from the CMS style messages dashboard. As you'd expect, they're also presence aware - so users only see them when they should.</p>
+								<section id="chat-features" class="row">
+			            		<div class="col-md-4">
+			            		    <img src="images/v/bell.svg" alt="feature_name" data-no-retina/>
+			            		    <h4>Alerts</h4>
+			                        <p>Send alerts to your users whenever you want - whether it be a product update, a warning notification or just to say happy birthday - it's available.</p>
+			                    </div>
+			            		<div class="col-md-4">
+			            		    <img src="images/v/code.svg" alt="feature_name" data-no-retina/>
+			            		    <h4>Custom messages</h4>
+			                        <p>Customise messages from chatting friends with the aid of merge tags, badge counter customisation and more. You can convey exactly the message you want.</p>
+			            		</div>
+			            		<div class="col-md-4">
+			            		    <img src="images/v/dash.svg" alt="feature_name" data-no-retina/>
+			            		    <h4>Dashboard</h4>
+			                        <p>Configure your push settings, send alerts to your users and view your past messages through the easy-to-use Quickblox CMS-style dashboard</p>
+			            		</div>
+			        		</section>
+						</section>
+					</div>
+					<div class="col-md-4">
+						<div id="push-iphone-wrap">
+							<img src="images/notifications-iphone.png" alt="A picture of push notifications appearing on an iPhone lock screen" id="notification-iphone"/>
+						</div>
+					</div>
+			    </div>
+		    </div>
+		</div>
+		
+		<div id="webapp-wrap"></div>
+		
+		<div id="enterprise-wrap">
+    		<div class="container">
+    			<div id="enterprise-chat-icon">
+	    			<img src="images/v/enterprise.svg" alt="Q-municate Enterprise" />
+    			</div>
+		        <div class="row">
+		    		<div class="col-md-12">
+						<section id="enterprise-content-wrap" class="content">
+							<h1>Q-municate for Enterprise</h1>
+							<p>The enterprise version of the Q-municate project is a parallel codebase licensed under proprietary license enabling wider commercial use. The functional differences include add-ons for security, compliance, data protection management and 3rd party software integrations. This comes with QuickBlox Enterprise subscription including professional enterprise level SLA, your dedicated DevOps manager, phone support team on duty and full 24/7/365 maintenance of your dedicated servers running QuickBlox cloud communication backend.</p>
+								<section id="chat-features" class="row">
+			            		<div class="col-md-4">
+			            		    <img src="images/v/security.svg" alt="feature_name" data-no-retina/>
+			            		    <h4>Extra Security</h4>
+			                        <p><ul>
+										<li>Client and server side encryption</li>
+										<li>Code obfuscation</li>
+										<li>Device ID database lock </li>
+										<li>“secret chat”</li>
+			                        </ul></p>
+			                    </div>
+			            		<div class="col-md-4">
+			            		    <img src="images/v/features.svg" alt="feature_name" data-no-retina/>
+			            		    <h4>More Features</h4>
+			                        <p><ul>				                        
+				                        <li>Contact lists management, existing SSO and user management integration</li>
+										<li>Add-ons and integrations (SharePoint, Microsoft Lync, AirWatch etc) </li>
+										<li>WebRTC - SIP VoIP integration and SIP gateway enabling to save money on international calls and SMS</li>
+										<li>Chat moderation, auto-filtering, abuse and data protection compliance (DPA, HIPPA etc.)</li>
+			                        </ul></p>
+			            		</div>
+			            		<div class="col-md-4">
+			            		    <img src="images/v/enterprise-ready.svg" alt="feature_name" data-no-retina/>
+			            		    <h4>Enterprise Ready</h4>
+			                        <p><ul>
+				                        <li>Commercial license (no community license limitations)</li>
+										<li>Consultancy package: our developers assist with integration / white labelling</li>
+										<li>Your dedicated server setup combining the scalability of cloud and security of owning your servers</li>
+										<li>Monthly support allowance + infrastructure optimisation and security consulting from your DevOp manager</li>
+										<li>Full access to source codes (including QB SDK binary libraries), optional server-side code escrow</li>
+										<li>"Super user” dashboard access (user management, SIP portal), better monitoring, detailed billing and stats</li>
+			                        </ul></p>
+			            		</div>
+			        		</section>
+						</section>
+					</div>
+			    </div>
+		    </div>
+		</div>
+		
+		<div id="get-beta-top"></div>
+		
+		<div id="get-beta">
+		    <div class="container">
+		        <div id="beta-form-wrap" class="row">
+        			<h1>Get the source</h1>
+        			<form id="beta-form">
+        				<div class="row">
+        					<div class="col-xs-6">
+        						<label>
+        							Full name
+        							<input type="text" id="first_name" placeholder="First name" required/>
+        						</label>
+        					</div>
+        					<div class="col-xs-6">
+        						<label>
+        							Last name
+        							<input type="text" id="last_name" placeholder="Last name" required/>
+        						</label>
+        					</div>
+        				</div>
+        				<div class="row">
+        					<div class="col-xs-12">
+        						<label>
+        							Company
+        							<input type="text" id="company" placeholder="Company" required/>
+        						</label>
+        					</div>
+        				</div>
+        				<div class="row">
+        					<div class="col-xs-12">
+        						<label>
+        							Email
+        							<input type="email" id="email" placeholder="Email address" required/>
+        						</label>
+        					</div>
+        				</div>
+        				<div class="row">
+        					<div class="col-xs-12">
+        						<a href="license.txt">Please read the license.</a>
+        						<button type="submit" id="send-request" class="pull-right">Get Q-municate</button>
+        					</div>
+        				</div>
+        			</form>
+		        </div>
+		    </div>
+		</div>
+		
+		<div id="get-beta-bottom"></div>
+		
+		<footer id="footer">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-4">
+						<a href="http://quickblox.com/" class="discreet"><img src="images/poweredbyqb.png" alt="Powered by Quickblox" id="footer-logo"/></a>
+					</div>
+					<div class="col-xs-4 text-center">
+						<img src="images/mrquick.png" alt="Mr Quick (QuickBlox mascot)"/>
+					</div>
+					<div class="col-xs-4 text-right">
+						<div id="copyright">&copy; <a href="http://quickblox.com/" class="discreet">QuickBlox</a> <?php echo date("Y"); ?></div>
+					</div>
+				</div>
+			</div>
+		</footer>
+		
+	</div>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script>
+
+	$(document).ready(function() {
+		$("#welcome .join-beta").click(function() {
+			$('html,body').animate({scrollTop: ($("#first_name").offset().top - 50)}, 1000);
+	        $("#first_name").focus();
+		});
+	});
+	if(localStorage.QMHasRegisteredForBeta) {
+		thanks();
+	}
+    $("#beta-form").submit(function(e) {
+	    e.preventDefault();
+        $("#send-request").prop("disabled", true);
+	    var fname = $("#first_name").val(),
+	    	lname = $("#last_name").val(),
+	    	company = $("#company").val(),
+	    	email = $("#email").val();
+	    	
+	    if(fname === "" || lname === "" || company === "" || email === "" || email.indexOf("@") === -1) {
+		    alert("All fields are required!");
+	    } else {
+	        if(fname.indexOf(";") === -1 && lname.indexOf(";") === -1 && company.indexOf(";") === -1) {
+    		    $.ajax({
+                    url:"http://node.quickblox.com/qm/reg",
+                    type:"POST",
+                    crossDomain:true,
+                    dataType:"json",
+                    data: {first_name: fname, last_name: lname, company: company, email: email},
+                    success:thanks,
+                    error:function(){alert("Oh no! We weren't able to register you. Please check all your details and try again, or send an email to q-municate@quickblox.com"); $("#send-request").prop("disabled", false);}
+                });
+            } else {
+                alert("Last time I checked, names didn't have semi-colons. ;-)");
+            }
+	    }
+	    
+    });
+    function thanks() {
+        localStorage.QMHasRegisteredForBeta = true;
+	    $("#get-beta form").remove();
+		$("#get-beta h1").text("Thanks for registering for the beta. Look out for the email soon!");
+		$("#get-beta h1").css({"line-height": "1.2em", margin: "60px 0", color: "#00C72D"});
+    }
+    
+</script>
+<script>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	
+	ga('create', 'UA-26728270-5', 'q-municate.com');
+	ga('send', 'pageview');
+</script>
+</body>
+</html>
